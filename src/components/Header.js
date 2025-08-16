3a14527cfb8b1e944ac7dd5ac40608d59764065f
cgpvibe <cgpvibe@gmail.com>
2025-08-17 03:40:46 +0900

    dd

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from './Logo'
 
 export default function Header() {
   return (
-    <header className='w-full bg-black/0 fixed top-0 left-0 backdrop-blur-lg z-20 mix-blend-difference'>
+    <header className='w-full bg-black/0 fixed top-0 left-0 backdrop-blur-none z-20 mix-blend-difference'>
       <div className='flex justify-between items-center h-16 bg-white/0 px-5'>
         <Logo width={126} height={20} />
         <div>Menu</div>
