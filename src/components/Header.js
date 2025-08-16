018ef59497502d0170f0643170eae2a20ec9ccfd
cgpvibe <cgpvibe@gmail.com>
2025-08-16 15:23:46 +0900

    nvm use

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from './Logo'
 
 export default function Header() {
   return (
-    <header className='w-full bg-black/50 fixed top-0 left-0'>
+    <header className='w-full bg-black/50 fixed top-0 left-0 backdrop-blur-2xl'>
       <div className='container mx-auto flex justify-between items-center h-16 bg-white/0'>
         <Logo width={126} height={20} />
         <div>Menu</div>
